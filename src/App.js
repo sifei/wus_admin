0c89889ca4248b6a03723c8e72d5db873b131c0f
sifei <sifei.han@gmail.com>
2022-09-19 16:20:43 -0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
-import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-} from "@aws-amplify/ui-react";
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+import logo from './logo.svg';
+import {Amplify, Auth, Logger} from 'aws-amplify';
+import React, {useEffect, useState} from "react";
+import {Authenticator, useAuthenticator} from '@aws-amplify/ui-react';
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
+import {SelectField, TextField} from '@aws-amplify/ui-react';
 
-function App({ signOut }) {
+
+Amplify.configure(awsExports);
+
+function App() {
+  const [username,setUsername] = useState("")
+  const funcc = async () => {
+    let user = await Auth.currentAuthenticatedUser();
+    console.log(user)
+    const { username } = user;
+    setUsername(username)
+  }
+  funcc()
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <h3 id="userName"> Welcome {username} </h3>
+        </div>
+        <p></p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://tsui-wakeupsafe.research.chop.edu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wakeup Safe
+        </a>
+
+      </header>
+
+    </div>
   );
 }
 
